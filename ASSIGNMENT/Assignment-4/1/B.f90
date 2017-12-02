918485af8af9f5362a01915222b7a9b5f52fb11b
karigor <ahmedshuvo969@gamil.com>
2017-12-03 01:59:57 +0600

    Add files via upload

---
+++ ASSIGNMENT/Assignment-4/1/B.f90
@@ -0,0 +1,11 @@
+I=0
+J=1
+K=1
+DO M=1,100
+    PRINT*,K
+    K=I+J
+    I=J
+    J=K
+    IF(K>100)GOTO 10
+END DO
+10 END
---
+++ ASSIGNMENT/Assignment-4/1/C.f90
@@ -0,0 +1,11 @@
+READ*,N
+I=0
+J=1
+K=1
+DO M=1,N
+    PRINT*,K
+    K=I+J
+    I=J
+    J=K
+END DO
+10 END
