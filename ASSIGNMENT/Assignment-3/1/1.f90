49965fdc97fdf5e8c008389d1850f27de2e130d1
karigor <ahmedshuvo969@gamil.com>
2017-12-03 01:41:58 +0600

    Add files via upload

---
+++ ASSIGNMENT/Assignment-3/1/1.f90
@@ -0,0 +1,8 @@
+AMOUNT=3000
+DO I=1,40
+    IF(I<=12)PRINT*,"  Month=",I,"Amount=",AMOUNT
+    IF(AMOUNT<250)GOTO 10
+    AMOUNT=AMOUNT+AMOUNT*0.075-250
+END DO
+10 PRINT*,"Month=",I,"Amount of last payment =",AMOUNT
+END
